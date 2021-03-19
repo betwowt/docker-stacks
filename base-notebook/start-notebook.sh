192706997c4b88fcbcf9527a294c33c0dcd0a17f
zhengf.l <zfliu@nanshudata.com>
2021-03-19 10:06:17 +0800

    modify

--- base-notebook/start-notebook.sh
+++ base-notebook/start-notebook.sh
@@ -14,10 +14,6 @@ if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
     exec /usr/local/bin/start-singleuser.sh "$@"
 elif [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
     . /usr/local/bin/start.sh $wrapper jupyter lab "$@"
-    echo -e "\nc.NotebookApp.token = ''\n" >> $HOME/.jupyter/jupyter_notebook_config.py
-    if [[ ! -z "${BASE_URL}" ]]; then
-        echo -e "c.NotebookApp.base_url = = '$BASE_URL'" >> $HOME/.jupyter/jupyter_notebook_config.py
-    fi
 else
     echo "WARN: Jupyter Notebook deprecation notice https://github.com/jupyter/docker-stacks#jupyter-notebook-deprecation-notice."
     . /usr/local/bin/start.sh $wrapper jupyter notebook "$@"
